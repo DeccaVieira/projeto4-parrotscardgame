3f50f52eb2463097768de507f359ce5d9d35b5ac
Andreia Vieira Gomes <andreiavieira2353@gmail.com>
2022-08-18 17:44:08 -0300

    Layout e qtdeCartas

---
+++ script.js
@@ -0,0 +1,15 @@
+const qtdeCartas = prompt("Digite a quantidade de cartas que deseja jogar:");
+
+function QuantidadeCartas(){
+    if(qtdeCartas <= 14 && qtdeCartas>=2 && qtdeCartas%2==0){
+console.log("é par");
+    }
+QuantidadeCartas(qtdeCartas);
+
+const cartas = [];
+while(cartas < qtdeCartas){
+    
+}
+//garantir que o usuário digite um número par
+//criar um array que puxe a quantidade de elementos que o usuário digitou - 1 com o push
+//alinhar esses elementos com o justify-content
